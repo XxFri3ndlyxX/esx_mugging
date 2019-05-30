a2577144432edc8f5f0fb94bf1c615c2d5be1db0
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2019-05-30 00:23:37 -0400

    moved item to config

--- esx_mugging/config.lua
+++ esx_mugging/config.lua
@@ -1,6 +1,4 @@
 Config = {}
-Config.Locale = 'en'
-
 
 Config.MinMoney = 5               --  Min amount of money received from mugging
 Config.MaxMoney = 100               --  Max amount of money received from mugging.
@@ -10,3 +8,14 @@ Config.AddItemsMax = 3              --  This is the max number of items you can 
 
 Config.PoliceNotify = 40            --  This is percent the police will get notified: example: 40 means 40% chance of notification.
 Config.AlwaysNotifyonDeath = true   --  This will set to 100% notify if NPC dies while someone is mugging them.
+
+Config.giveableItems = {
+  'weed',
+	'water',
+	'hamburger',
+	'lighter',
+	'coke',
+	'vodka',
+	'meth',
+	'ecola'
+}
